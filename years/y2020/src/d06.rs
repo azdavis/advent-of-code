59da3afa78220cbd53a107a8403ff34a9a5b8f00
Ariel Davis <ariel.z.davis@icloud.com>
2020-12-10 00:21:00 -0800

    Do y2020 d6 p2

--- years/y2020/src/d06.rs
+++ years/y2020/src/d06.rs
@@ -11,3 +11,21 @@ pub fn p1(s: &str) -> usize {
     })
     .sum()
 }
+
+pub fn p2(s: &str) -> usize {
+  s.split("\n\n")
+    .map(|group| {
+      let mut sets = group.split('\n').filter_map(|line| {
+        if line.is_empty() {
+          None
+        } else {
+          Some(line.chars().collect::<HashSet<_>>())
+        }
+      });
+      let fst = sets.next().unwrap();
+      sets
+        .fold(fst, |ac, x| ac.intersection(&x).copied().collect())
+        .len()
+    })
+    .sum()
+}
