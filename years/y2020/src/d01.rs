e0f4d46db54e229704a6173387dc4f63ac321436
Ariel Davis <ariel.z.davis@icloud.com>
2020-12-09 21:19:16 -0800

    Do part 1

--- years/y2020/src/d01.rs
+++ years/y2020/src/d01.rs
@@ -1,7 +1,34 @@
+const TARGET: u32 = 2020;
+
+fn parse(s: &str) -> Vec<u32> {
+  s.split_ascii_whitespace()
+    .map(str::parse)
+    .collect::<Result<_, _>>()
+    .unwrap()
+}
+
 pub fn p1(s: &str) {
-  todo!("{}", s)
+  let xs = parse(s);
+  for &a in xs.iter() {
+    for &b in xs.iter() {
+      if a + b == TARGET {
+        println!("{}", a * b);
+        return;
+      }
+    }
+  }
 }
 
 pub fn p2(s: &str) {
-  todo!("{}", s)
+  let xs = parse(s);
+  for &a in xs.iter() {
+    for &b in xs.iter() {
+      for &c in xs.iter() {
+        if a + b + c == TARGET {
+          println!("{}", a * b * c);
+          return;
+        }
+      }
+    }
+  }
 }
