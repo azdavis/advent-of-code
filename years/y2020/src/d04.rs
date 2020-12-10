098cae59e257a08bdfcb5d4347a2f0829c77461f
Ariel Davis <ariel.z.davis@icloud.com>
2020-12-09 22:45:13 -0800

    Parse keys into an enum

--- years/y2020/src/d04.rs
+++ years/y2020/src/d04.rs
@@ -6,24 +6,60 @@ pub fn p1(s: &str) {
 
 fn help<F>(s: &str, f: F)
 where
-  F: Fn(&str, &str) -> bool,
+  F: Fn(Key, &str) -> bool,
 {
   let mut ok = 0;
   'outer: for passport in s.split("\n\n") {
-    let mut need = hashset!["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+    let mut need = hashset![
+      Key::BirthYear,
+      Key::IssueYear,
+      Key::ExpirationYear,
+      Key::Height,
+      Key::HairColor,
+      Key::EyeColor,
+      Key::PassportID,
+    ];
     for field in passport.split_ascii_whitespace() {
       let mut iter = field.split(':');
-      let key = iter.next().unwrap();
+      let key = Key::parse(iter.next().unwrap()).unwrap();
       let val = iter.next().unwrap();
       assert!(iter.next().is_none());
       if !f(key, val) {
         continue 'outer;
       }
-      need.remove(key);
+      need.remove(&key);
     }
     if need.is_empty() {
       ok += 1;
     }
   }
   println!("{}", ok);
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+enum Key {
+  BirthYear,
+  IssueYear,
+  ExpirationYear,
+  Height,
+  HairColor,
+  EyeColor,
+  PassportID,
+  CountryID,
+}
+
+impl Key {
+  fn parse(s: &str) -> Option<Key> {
+    match s {
+      "byr" => Some(Self::BirthYear),
+      "iyr" => Some(Self::IssueYear),
+      "eyr" => Some(Self::ExpirationYear),
+      "hgt" => Some(Self::Height),
+      "hcl" => Some(Self::HairColor),
+      "ecl" => Some(Self::EyeColor),
+      "pid" => Some(Self::PassportID),
+      "cid" => Some(Self::CountryID),
+      _ => None,
+    }
+  }
 }
