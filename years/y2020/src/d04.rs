be577b2cc61687b3ce8bf94006c33df41877f8cb
Ariel Davis <ariel.z.davis@icloud.com>
2020-12-09 22:38:10 -0800

    Make general

--- years/y2020/src/d04.rs
+++ years/y2020/src/d04.rs
@@ -1,12 +1,25 @@
 use maplit::hashset;
 
 pub fn p1(s: &str) {
+  help(s, |_, _| true)
+}
+
+fn help<F>(s: &str, f: F)
+where
+  F: Fn(&str, &str) -> bool,
+{
   let mut ok = 0;
-  for passport in s.split("\n\n") {
+  'outer: for passport in s.split("\n\n") {
     let mut need = hashset!["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
     for field in passport.split_ascii_whitespace() {
-      let field = field.split(':').next().unwrap();
-      need.remove(field);
+      let mut iter = field.split(':');
+      let key = iter.next().unwrap();
+      let val = iter.next().unwrap();
+      assert!(iter.next().is_none());
+      if !f(key, val) {
+        continue 'outer;
+      }
+      need.remove(key);
     }
     if need.is_empty() {
       ok += 1;
