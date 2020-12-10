ebaf0d8d5f02b974993843ef9a115da6f0116b42
Ariel Davis <ariel.z.davis@icloud.com>
2020-12-10 00:15:34 -0800

    Avoid a temporary

--- years/y2020/src/d06.rs
+++ years/y2020/src/d06.rs
@@ -3,9 +3,11 @@ use std::collections::HashSet;
 pub fn p1(s: &str) -> usize {
   s.split("\n\n")
     .map(|group| {
-      let any_yes: HashSet<_> =
-        group.split('\n').flat_map(|line| line.chars()).collect();
-      any_yes.len()
+      group
+        .split('\n')
+        .flat_map(|line| line.chars())
+        .collect::<HashSet<_>>()
+        .len()
     })
     .sum()
 }
