698e3019b48f74ee9cb05c1a432a1c6546a18629
Ariel Davis <ariel.z.davis@icloud.com>
2020-12-09 23:15:31 -0800

    Do y2020 d4 p2

--- years/y2020/src/d04.rs
+++ years/y2020/src/d04.rs
@@ -1,7 +1,54 @@
 use maplit::hashset;
+use once_cell::sync::Lazy;
+use regex::Regex;
 
 pub fn p1(s: &str) {
   help(s, |_, _| true)
+}
+
+pub fn p2(s: &str) {
+  help(s, is_p2_valid_key)
+}
+
+fn is_num_in_range(val: &str, lo: u32, hi: u32) -> bool {
+  match val.parse::<u32>() {
+    Ok(n) => lo <= n && n <= hi,
+    Err(_) => false,
+  }
+}
+
+static HEIGHT: Lazy<Regex> = Lazy::new(|| Regex::new(r"^(\d+)(\w+)$").unwrap());
+static HAIR_COLOR: Lazy<Regex> =
+  Lazy::new(|| Regex::new(r"^#[0-9a-f]{6}$").unwrap());
+static PASSPORT_ID: Lazy<Regex> = Lazy::new(|| Regex::new(r"^\d{9}$").unwrap());
+
+fn is_p2_valid_key(key: Key, val: &str) -> bool {
+  match key {
+    Key::BirthYear => is_num_in_range(val, 1920, 2002),
+    Key::IssueYear => is_num_in_range(val, 2010, 2020),
+    Key::ExpirationYear => is_num_in_range(val, 2020, 2030),
+    Key::Height => {
+      let caps = match HEIGHT.captures(val) {
+        Some(x) => x,
+        None => return false,
+      };
+      let n: u32 = match caps[1].parse() {
+        Ok(x) => x,
+        Err(_) => return false,
+      };
+      match &caps[2] {
+        "cm" => 150 <= n && n <= 193,
+        "in" => 59 <= n && n <= 76,
+        _ => false,
+      }
+    }
+    Key::HairColor => HAIR_COLOR.is_match(val),
+    Key::EyeColor => {
+      matches!(val, "amb" | "blu" | "brn" | "gry" | "grn" | "hzl" | "oth")
+    }
+    Key::PassportID => PASSPORT_ID.is_match(val),
+    Key::CountryID => true,
+  }
 }
 
 fn help<F>(s: &str, f: F)
