720a694ece5b7570d335416e2c711bc33ade0f3c
Ariel Davis <ariel.z.davis@icloud.com>
2020-12-09 21:59:37 -0800

    Just hard-code which day to run

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,27 +1,9 @@
-mod env;
-mod selection;
-
-use selection::SelectionError;
 use std::io::Read as _;
 
-fn get_stdin() -> Result<String, std::io::Error> {
-  let mut buf = String::new();
-  std::io::stdin().read_to_string(&mut buf)?;
-  Ok(buf)
-}
-
 fn run() -> Result<(), Box<dyn std::error::Error>> {
-  let year: u16 = env::var("AOC_YEAR")?.parse()?;
-  let day: u16 = env::var("AOC_DAY")?.parse()?;
-  let part: u16 = env::var("AOC_PART")?.parse()?;
-  let inp = get_stdin()?;
-  match (year, day, part) {
-    (2020, 1, 1) => y2020::d01::p1(&inp),
-    (2020, 1, 2) => y2020::d01::p2(&inp),
-    (2020, 2, 1) => y2020::d02::p1(&inp),
-    (2020, 2, 2) => y2020::d02::p2(&inp),
-    _ => return Err(SelectionError { year, day, part }.into()),
-  }
+  let mut inp = String::new();
+  std::io::stdin().read_to_string(&mut inp)?;
+  y2020::d02::p2(&inp);
   Ok(())
 }
 
