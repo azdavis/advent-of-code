2e55e4eabfb9e53365ff8f8879d45556ea9549b2
Ariel Davis <ariel.z.davis@icloud.com>
2020-12-09 22:22:59 -0800

    Make general

--- years/y2020/src/d03.rs
+++ years/y2020/src/d03.rs
@@ -1,8 +1,13 @@
 pub fn p1(s: &str) {
+  println!("{}", help(s, 3, 1));
+}
+
+fn help(s: &str, right: usize, down: usize) -> usize {
   let mut trees = 0;
   let mut y = 0;
   let line_len = s.split('\n').next().unwrap().len();
-  for line in s.split('\n') {
+  let mut iter = s.split('\n');
+  while let Some(line) = iter.next() {
     if line.is_empty() {
       continue;
     }
@@ -10,8 +15,11 @@ pub fn p1(s: &str) {
     if bs[y] == b'#' {
       trees += 1;
     }
-    y += 3;
+    y += right;
     y %= line_len;
+    for _ in 0..down - 1 {
+      iter.next();
+    }
   }
-  println!("{}", trees);
+  trees
 }
