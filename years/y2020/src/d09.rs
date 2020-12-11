90021d249ba6f09f16df111c3b1fce1cb6488682
Ariel Davis <ariel.z.davis@icloud.com>
2020-12-10 20:17:46 -0800

    Do y2020 d9 p1

---
+++ years/y2020/src/d09.rs
@@ -0,0 +1,28 @@
+pub fn p1(s: &str) -> u64 {
+  let nums = parse(s);
+  'outer: for (idx, &n) in nums.iter().enumerate().skip(WINDOW) {
+    for &a in nums[idx - WINDOW..idx].iter() {
+      for &b in nums[idx - WINDOW..idx].iter() {
+        if a + b == n {
+          continue 'outer;
+        }
+      }
+    }
+    return n;
+  }
+  panic!()
+}
+
+const WINDOW: usize = 25;
+
+fn parse(s: &str) -> Vec<u64> {
+  s.split('\n')
+    .filter_map(|x| {
+      if x.is_empty() {
+        None
+      } else {
+        Some(x.parse().unwrap())
+      }
+    })
+    .collect()
+}
