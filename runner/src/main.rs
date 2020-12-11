c51b80d2bb64c0371399eca9a1ec699b6026b6c9
Ariel Davis <ariel.z.davis@icloud.com>
2020-12-11 00:17:43 -0800

    Get input from committed input

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,4 +1,4 @@
 fn main() {
-  let inp = std::fs::read_to_string("input/input.txt").unwrap();
+  let inp = include_str!("../../years/y2020/src/input/11.txt");
   println!("{}", y2020::d11::p2(&inp));
 }
