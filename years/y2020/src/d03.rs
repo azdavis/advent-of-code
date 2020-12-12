e115a768bc6941e9dbe8215ed2c6d4262c94d174
Ariel Davis <ariel.z.davis@icloud.com>
2020-12-11 17:03:29 -0800

    Parse

--- years/y2020/src/d03.rs
+++ years/y2020/src/d03.rs
@@ -1,22 +1,24 @@
 pub fn p1(s: &str) -> usize {
-  help(s, 3, 1)
+  let grid = parse(s);
+  help(&grid, 3, 1)
 }
 
 pub fn p2(s: &str) -> usize {
-  help(s, 1, 1) * help(s, 3, 1) * help(s, 5, 1) * help(s, 7, 1) * help(s, 1, 2)
+  let grid = parse(s);
+  help(&grid, 1, 1)
+    * help(&grid, 3, 1)
+    * help(&grid, 5, 1)
+    * help(&grid, 7, 1)
+    * help(&grid, 1, 2)
 }
 
-fn help(s: &str, right: usize, down: usize) -> usize {
+fn help(grid: &[&[u8]], right: usize, down: usize) -> usize {
   let mut ret = 0;
   let mut y = 0;
-  let line_len = s.split('\n').next().unwrap().len();
-  let mut iter = s.split('\n');
-  while let Some(line) = iter.next() {
-    if line.is_empty() {
-      continue;
-    }
-    let bs = line.as_bytes();
-    if bs[y] == b'#' {
+  let line_len = grid.first().unwrap().len();
+  let mut iter = grid.iter();
+  while let Some(row) = iter.next() {
+    if row[y] == b'#' {
       ret += 1;
     }
     y += right;
@@ -26,4 +28,11 @@ fn help(s: &str, right: usize, down: usize) -> usize {
     }
   }
   ret
+}
+
+fn parse(s: &str) -> Vec<&[u8]> {
+  s.split('\n')
+    .filter(|line| !line.is_empty())
+    .map(|line| line.as_bytes())
+    .collect()
 }
