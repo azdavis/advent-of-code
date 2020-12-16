9cba6a1f08368039b0be398d275bb6ad19d0bac8
Ariel Davis <ariel.z.davis@icloud.com>
2020-12-15 17:33:55 -0800

    Do y2019 d4 p1

--- years/y2019/src/d04.rs
+++ years/y2019/src/d04.rs
@@ -1,14 +1,42 @@
-pub fn p1(_: &str) -> usize {
-  todo!()
+use std::cmp::Ordering;
+
+pub fn p1(s: &str) -> usize {
+  let (lo, hi) = parse(s);
+  (lo..=hi).filter(|&x| is_valid(x)).count()
 }
 
 pub fn p2(_: &str) -> usize {
   todo!()
 }
 
+fn is_valid(n: u32) -> bool {
+  if n < 100000 || n > 999999 {
+    return false;
+  }
+  let mut ds = helpers::digits::digits(n);
+  let mut prev = ds.next().unwrap();
+  let mut has_double = false;
+  for d in ds {
+    match d.cmp(&prev) {
+      Ordering::Less => return false,
+      Ordering::Equal => has_double = true,
+      Ordering::Greater => {}
+    }
+    prev = d;
+  }
+  has_double
+}
+
+fn parse(s: &str) -> (u32, u32) {
+  let mut parts = s.split('\n').next().unwrap().split('-');
+  let lo: u32 = parts.next().unwrap().parse().unwrap();
+  let hi: u32 = parts.next().unwrap().parse().unwrap();
+  (lo, hi)
+}
+
 #[test]
 fn t() {
   let inp = include_str!("input/d04.txt");
-  assert_eq!(p1(inp), 123);
-  assert_eq!(p2(inp), 123);
+  assert_eq!(p1(inp), 1864);
+  // assert_eq!(p2(inp), 123);
 }
