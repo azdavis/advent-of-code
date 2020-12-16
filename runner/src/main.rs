bb88ddf37f77ad604a84fc244ef09f09c6a07e83
Ariel Davis <ariel.z.davis@icloud.com>
2020-12-15 17:43:29 -0800

    Do y2019 d4 p2

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,4 +1,4 @@
 fn main() {
   let inp = include_str!("../../years/y2019/src/input/d04.txt");
-  println!("{}", y2019::d04::p1(&inp));
+  println!("{}", y2019::d04::p2(&inp));
 }
--- years/y2019/src/d04.rs
+++ years/y2019/src/d04.rs
@@ -2,14 +2,15 @@ use std::cmp::Ordering;
 
 pub fn p1(s: &str) -> usize {
   let (lo, hi) = parse(s);
-  (lo..=hi).filter(|&x| is_valid(x)).count()
+  (lo..=hi).filter(|&n| is_valid_p1(n)).count()
 }
 
-pub fn p2(_: &str) -> usize {
-  todo!()
+pub fn p2(s: &str) -> usize {
+  let (lo, hi) = parse(s);
+  (lo..=hi).filter(|&n| is_valid_p2(n)).count()
 }
 
-fn is_valid(n: u32) -> bool {
+fn is_valid_p1(n: u32) -> bool {
   if n < 100000 || n > 999999 {
     return false;
   }
@@ -27,6 +28,39 @@ fn is_valid(n: u32) -> bool {
   has_double
 }
 
+fn is_valid_p2(n: u32) -> bool {
+  if n < 100000 || n > 999999 {
+    return false;
+  }
+  let mut ds = helpers::digits::digits(n);
+  let mut prev = ds.next().unwrap();
+  let mut has_double = false;
+  let mut run = Run::One;
+  for d in ds {
+    run = match d.cmp(&prev) {
+      Ordering::Less => return false,
+      Ordering::Equal => match run {
+        Run::One => Run::Two,
+        Run::Two | Run::Big => Run::Big,
+      },
+      Ordering::Greater => {
+        if matches!(run, Run::Two) {
+          has_double = true;
+        }
+        Run::One
+      }
+    };
+    prev = d;
+  }
+  has_double || matches!(run, Run::Two)
+}
+
+enum Run {
+  One,
+  Two,
+  Big,
+}
+
 fn parse(s: &str) -> (u32, u32) {
   let mut parts = s.split('\n').next().unwrap().split('-');
   let lo: u32 = parts.next().unwrap().parse().unwrap();
@@ -38,5 +72,5 @@ fn parse(s: &str) -> (u32, u32) {
 fn t() {
   let inp = include_str!("input/d04.txt");
   assert_eq!(p1(inp), 1864);
-  // assert_eq!(p2(inp), 123);
+  assert_eq!(p2(inp), 1258);
 }
