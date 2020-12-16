12d3d3a0a59727513999b9779ebc8a6aa3a7dced
Ariel Davis <ariel.z.davis@icloud.com>
2020-12-15 17:52:12 -0800

    Do y2019 d5 p1

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,4 +1,4 @@
 fn main() {
-  let inp = include_str!("../../years/y2019/src/input/d04.txt");
-  println!("{}", y2019::d04::p2(&inp));
+  let inp = include_str!("../../years/y2019/src/input/d05.txt");
+  println!("{}", y2019::d05::p1(&inp));
 }
---
+++ years/y2019/src/d05.rs
@@ -0,0 +1,93 @@
+use std::convert::TryInto as _;
+
+pub fn p1(s: &str) -> i32 {
+  let ns = parse(s);
+  let mut output = Vec::new();
+  go(ns, &[1], &mut output);
+  let code = output.pop().unwrap();
+  for x in output {
+    assert_eq!(x, 0);
+  }
+  code
+}
+
+pub fn p2(s: &str) -> i32 {
+  todo!()
+}
+
+fn go(mut ns: Vec<i32>, input: &[i32], output: &mut Vec<i32>) {
+  let mut input = input.iter().copied();
+  let mut idx = 0;
+  loop {
+    let cur = ns[idx];
+    let op = cur % 100;
+    let modes = cur / 100;
+    match op {
+      1 => {
+        let (a, b, c) = get_3_arg(&ns, idx, modes);
+        ns[c] = a + b;
+        idx += 4;
+      }
+      2 => {
+        let (a, b, c) = get_3_arg(&ns, idx, modes);
+        ns[c] = a * b;
+        idx += 4;
+      }
+      3 => {
+        let a = get_pos_arg(&ns, idx + 1, modes);
+        ns[a] = input.next().unwrap();
+        idx += 2;
+      }
+      4 => {
+        let a = get_arg(&ns, idx + 1, modes);
+        output.push(a);
+        idx += 2;
+      }
+      99 => break,
+      _ => panic!("invalid op: {}", op),
+    }
+  }
+}
+
+fn get_arg(ns: &[i32], idx: usize, mode: i32) -> i32 {
+  let val = ns[idx];
+  match mode {
+    0 => ns[u(val)],
+    1 => val,
+    _ => panic!("invalid mode: {}", mode),
+  }
+}
+
+fn get_pos_arg(ns: &[i32], idx: usize, mode: i32) -> usize {
+  assert_eq!(mode, 0);
+  u(ns[idx])
+}
+
+fn get_3_arg(ns: &[i32], idx: usize, mut modes: i32) -> (i32, i32, usize) {
+  let a = get_arg(&ns, idx + 1, modes % 10);
+  modes /= 10;
+  let b = get_arg(&ns, idx + 2, modes % 10);
+  modes /= 10;
+  let c = get_pos_arg(ns, idx + 3, modes);
+  (a, b, c)
+}
+
+fn u(n: i32) -> usize {
+  n.try_into().unwrap()
+}
+
+fn parse(s: &str) -> Vec<i32> {
+  s.split('\n')
+    .next()
+    .unwrap()
+    .split(',')
+    .map(|s| s.parse().unwrap())
+    .collect()
+}
+
+#[test]
+fn t() {
+  let inp = include_str!("input/d05.txt");
+  assert_eq!(p1(inp), 13210611);
+  // assert_eq!(p2(inp), ___);
+}
