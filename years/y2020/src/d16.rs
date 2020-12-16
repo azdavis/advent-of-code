60eef5769fe8da3c5578ac607d4f6270cf5aee6f
Ariel Davis <ariel.z.davis@icloud.com>
2020-12-15 23:00:40 -0800

    Do y2020 d16 p1

--- years/y2020/src/d16.rs
+++ years/y2020/src/d16.rs
@@ -1,14 +1,85 @@
+use std::collections::HashMap;
+use std::ops::RangeInclusive;
+
 pub fn p1(s: &str) -> u32 {
-  todo!()
+  let inp = parse(s);
+  inp
+    .nearby
+    .iter()
+    .flatten()
+    .filter(|&num| inp.fields.values().all(|ranges| !ranges.contains(num)))
+    .sum()
 }
 
 pub fn p2(s: &str) -> u32 {
   todo!()
 }
 
+#[derive(Debug)]
+struct Input<'a> {
+  fields: HashMap<&'a str, Ranges>,
+  me: Ticket,
+  nearby: Vec<Ticket>,
+}
+
+type Ticket = Vec<u32>;
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+struct Ranges {
+  fst: RangeInclusive<u32>,
+  snd: RangeInclusive<u32>,
+}
+
+impl Ranges {
+  fn contains(&self, num: &u32) -> bool {
+    self.fst.contains(num) || self.snd.contains(num)
+  }
+}
+
+fn parse(s: &str) -> Input {
+  let mut lines = s.split('\n');
+  let mut fields = HashMap::new();
+  loop {
+    let line = lines.next().unwrap();
+    if line.is_empty() {
+      break;
+    }
+    let mut parts = line.split(": ");
+    let key = parts.next().unwrap();
+    let mut parts = parts.next().unwrap().split(" or ");
+    let fst = parse_range(parts.next().unwrap());
+    let snd = parse_range(parts.next().unwrap());
+    fields.insert(key, Ranges { fst, snd });
+  }
+  assert_eq!(lines.next().unwrap(), "your ticket:");
+  let me = parse_ticket(lines.next().unwrap());
+  assert!(lines.next().unwrap().is_empty());
+  assert_eq!(lines.next().unwrap(), "nearby tickets:");
+  let mut nearby = Vec::new();
+  loop {
+    let line = lines.next().unwrap();
+    if line.is_empty() {
+      break;
+    }
+    nearby.push(parse_ticket(line));
+  }
+  Input { fields, me, nearby }
+}
+
+fn parse_ticket(s: &str) -> Ticket {
+  s.split(',').map(|x| x.parse().unwrap()).collect()
+}
+
+fn parse_range(s: &str) -> RangeInclusive<u32> {
+  let mut parts = s.split('-');
+  let fst: u32 = parts.next().unwrap().parse().unwrap();
+  let snd: u32 = parts.next().unwrap().parse().unwrap();
+  fst..=snd
+}
+
 #[test]
 fn t() {
   let inp = include_str!("input/d16.txt");
-  // assert_eq!(p1(inp), ___);
+  assert_eq!(p1(inp), 25984);
   // assert_eq!(p2(inp), ___);
 }
