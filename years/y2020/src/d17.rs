5df291f83d09faa26a5f5b6c38226aa19951cab4
Ariel Davis <ariel.z.davis@icloud.com>
2020-12-17 01:02:23 -0800

    Do y2020 d17 p1

--- years/y2020/src/d17.rs
+++ years/y2020/src/d17.rs
@@ -1,14 +1,71 @@
-pub fn p1(s: &str) -> u32 {
+use helpers::vec3::Vec3;
+use std::collections::HashSet;
+use std::convert::TryInto as _;
+
+pub fn p1(s: &str) -> usize {
+  let mut map = parse(s);
+  for _ in 0..6 {
+    map = map
+      .iter()
+      .flat_map(|&v| {
+        let mut ret = neighbors(v);
+        ret.push(v);
+        ret
+      })
+      .filter(|&v| {
+        let ns_on =
+          neighbors(v).into_iter().filter(|n| map.contains(n)).count();
+        matches!((map.contains(&v), ns_on), (true, 2) | (_, 3))
+      })
+      .collect();
+  }
+  map.len()
+}
+
+pub fn p2(s: &str) -> usize {
   todo!()
 }
 
-pub fn p2(s: &str) -> u32 {
-  todo!()
+fn neighbors(v: Vec3<i32>) -> Vec<Vec3<i32>> {
+  const LEN: usize = (3 * 3 * 3) - 1;
+  let Vec3(x, y, z) = v;
+  let mut ret = Vec::with_capacity(LEN);
+  for dx in -1..=1 {
+    for dy in -1..=1 {
+      for dz in -1..=1 {
+        let add = Vec3(x + dx, y + dy, z + dz);
+        if add == v {
+          continue;
+        }
+        ret.push(add)
+      }
+    }
+  }
+  assert_eq!(ret.len(), LEN);
+  ret
+}
+
+fn to_i32(n: usize) -> i32 {
+  n.try_into().unwrap()
+}
+
+fn parse(s: &str) -> HashSet<Vec3<i32>> {
+  s.split('\n')
+    .filter(|line| !line.is_empty())
+    .enumerate()
+    .flat_map(|(x, line)| {
+      line.chars().enumerate().filter_map(move |(y, c)| match c {
+        '#' => Some(Vec3(to_i32(x), to_i32(y), 0)),
+        '.' => None,
+        _ => panic!("bad char: {}", c),
+      })
+    })
+    .collect()
 }
 
 #[test]
 fn t() {
   let inp = include_str!("input/d17.txt");
-  // assert_eq!(p1(inp), ___);
+  assert_eq!(p1(inp), 271);
   // assert_eq!(p2(inp), ___);
 }
