fafc5a470863c652773fa1c498dab47380e063ca
Ariel Davis <ariel.z.davis@icloud.com>
2020-12-17 19:39:28 -0800

    Rm some comments

--- years/y2019/src/d05.rs
+++ years/y2019/src/d05.rs
@@ -2,7 +2,6 @@ use crate::intcode::{parse, Intcode};
 
 pub fn p1(s: &str) -> i32 {
   let mut p = Intcode::new(parse(s));
-  // we output a code and run at least one test
   let mut output = Vec::with_capacity(2);
   p.input(1);
   p.run(&mut output);
@@ -15,7 +14,6 @@ pub fn p1(s: &str) -> i32 {
 
 pub fn p2(s: &str) -> i32 {
   let mut p = Intcode::new(parse(s));
-  // just output the code
   let mut output = Vec::with_capacity(1);
   p.input(5);
   p.run(&mut output);
