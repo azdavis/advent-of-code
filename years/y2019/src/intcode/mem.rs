ee4b04912986927757280551686fbab22027d1c4
Ariel Davis <ariel.z.davis@icloud.com>
2020-12-18 17:49:15 -0800

    Use the default value for T for uninitialized mem

--- years/y2019/src/intcode/mem.rs
+++ years/y2019/src/intcode/mem.rs
@@ -1,23 +1,27 @@
+use std::collections::HashMap;
+
 #[derive(Debug, Clone)]
 pub struct Mem<T> {
-  inner: Vec<T>,
+  inner: HashMap<usize, T>,
 }
 
 impl<T> Mem<T> {
   pub fn new(vec: Vec<T>) -> Self {
-    Self { inner: vec }
+    Self {
+      inner: vec.into_iter().enumerate().collect(),
+    }
   }
 
   pub fn write(&mut self, addr: usize, val: T) {
-    self.inner[addr] = val;
+    self.inner.insert(addr, val);
   }
 }
 
 impl<T> Mem<T>
 where
-  T: Copy,
+  T: Copy + Default,
 {
   pub fn read(&self, addr: usize) -> T {
-    self.inner[addr]
+    self.inner.get(&addr).copied().unwrap_or_default()
   }
 }
