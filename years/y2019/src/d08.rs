b3410478881c29bd0bc6f4a78f1f1469a913ddbc
Ariel Davis <ariel.z.davis@icloud.com>
2020-12-18 17:09:39 -0800

    Use Pixel

--- years/y2019/src/d08.rs
+++ years/y2019/src/d08.rs
@@ -1,12 +1,18 @@
 pub fn p1(s: &str) -> usize {
   let ns = parse(s);
-  let least_zeroes = ns
+  let least_black = ns
     .chunks(WIDTH * HEIGHT)
-    .min_by_key(|xs| xs.iter().filter(|&&x| x == 0).count())
+    .min_by_key(|xs| xs.iter().filter(|&x| matches!(*x, Pixel::Black)).count())
     .unwrap();
-  let num_ones = least_zeroes.iter().filter(|&&x| x == 1).count();
-  let num_twos = least_zeroes.iter().filter(|&&x| x == 2).count();
-  num_ones * num_twos
+  let num_white = least_black
+    .iter()
+    .filter(|&x| matches!(*x, Pixel::White))
+    .count();
+  let num_transparent = least_black
+    .iter()
+    .filter(|&x| matches!(*x, Pixel::Transparent))
+    .count();
+  num_white * num_transparent
 }
 
 pub fn p2(s: &str) -> u32 {
@@ -16,13 +22,31 @@ pub fn p2(s: &str) -> u32 {
 const WIDTH: usize = 25;
 const HEIGHT: usize = 6;
 
-fn parse(s: &str) -> Vec<u32> {
+fn parse(s: &str) -> Vec<Pixel> {
   s.split('\n')
     .next()
     .unwrap()
     .chars()
-    .map(|x| x.to_digit(10).unwrap())
+    .map(Pixel::parse)
     .collect()
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Pixel {
+  Black,
+  White,
+  Transparent,
+}
+
+impl Pixel {
+  fn parse(c: char) -> Self {
+    match c {
+      '0' => Self::Black,
+      '1' => Self::White,
+      '2' => Self::Transparent,
+      _ => panic!("bad pixel: {}", c),
+    }
+  }
 }
 
 #[test]
