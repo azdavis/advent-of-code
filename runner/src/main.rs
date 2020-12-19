4b7e8c29d2359445eb48509e595af95992622d40
Ariel Davis <ariel.z.davis@icloud.com>
2020-12-18 17:25:51 -0800

    Do y2019 d8 p2
    I really thought the 'U' was a 'V' there for a while… wasted a few
    submissions D:

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,4 +1,4 @@
 fn main() {
   let inp = include_str!("../../years/y2019/src/input/d08.txt");
-  println!("{}", y2019::d08::p1(&inp));
+  println!("{}", y2019::d08::p2(&inp));
 }
--- years/y2019/src/d08.rs
+++ years/y2019/src/d08.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 pub fn p1(s: &str) -> usize {
   let ns = parse(s);
   let least_black = ns
@@ -15,8 +17,53 @@ pub fn p1(s: &str) -> usize {
   num_white * num_transparent
 }
 
-pub fn p2(s: &str) -> u32 {
-  todo!()
+#[allow(clippy::needless_range_loop)]
+pub fn p2(s: &str) -> Img {
+  let ns = parse(s);
+  let mut pixels = vec![vec![Pixel::Transparent; WIDTH]; HEIGHT];
+  for y in 0..HEIGHT {
+    for x in 0..WIDTH {
+      let mut layer = 0;
+      pixels[y][x] = loop {
+        match ns.get((layer * WIDTH * HEIGHT) + (y * WIDTH) + x) {
+          None => break Pixel::Transparent,
+          Some(&Pixel::Transparent) => layer += 1,
+          Some(&x) => break x,
+        }
+      };
+    }
+  }
+  Img { pixels }
+}
+
+#[derive(Debug)]
+pub struct Img {
+  pixels: Vec<Vec<Pixel>>,
+}
+
+impl fmt::Display for Img {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    let mut rows = self.pixels.iter();
+    if let Some(row) = rows.next() {
+      fmt_row(row, f)?;
+    }
+    for row in rows {
+      writeln!(f)?;
+      fmt_row(row, f)?;
+    }
+    Ok(())
+  }
+}
+
+fn fmt_row(row: &[Pixel], f: &mut fmt::Formatter<'_>) -> fmt::Result {
+  for px in row.iter() {
+    match *px {
+      Pixel::Black => write!(f, "█")?,
+      Pixel::White => write!(f, "░")?,
+      Pixel::Transparent => write!(f, " ")?,
+    }
+  }
+  Ok(())
 }
 
 const WIDTH: usize = 25;
@@ -31,7 +78,7 @@ fn parse(s: &str) -> Vec<Pixel> {
     .collect()
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Pixel {
   Black,
   White,
@@ -53,5 +100,15 @@ impl Pixel {
 fn t() {
   let inp = include_str!("input/d08.txt");
   assert_eq!(p1(inp), 2760);
-  // assert_eq!(p2(inp), ___);
+  use Pixel::{Black as B, White as W};
+  #[rustfmt::skip]
+  let p2_out = [
+    [B, W, W, B, B, B, W, W, B, B, W, B, B, W, B, W, W, W, W, B, W, W, W, B, B],
+    [W, B, B, W, B, W, B, B, W, B, W, B, B, W, B, W, B, B, B, B, W, B, B, W, B],
+    [W, B, B, W, B, W, B, B, B, B, W, B, B, W, B, W, W, W, B, B, W, W, W, B, B],
+    [W, W, W, W, B, W, B, W, W, B, W, B, B, W, B, W, B, B, B, B, W, B, B, W, B],
+    [W, B, B, W, B, W, B, B, W, B, W, B, B, W, B, W, B, B, B, B, W, B, B, W, B],
+    [W, B, B, W, B, B, W, W, W, B, B, W, W, B, B, W, W, W, W, B, W, W, W, B, B],
+  ];
+  assert_eq!(p2(inp).pixels, p2_out);
 }
