c2bbcb3c239bee10c6eed0be0f636ba35cfbef89
Ariel Davis <ariel.z.davis@icloud.com>
2020-12-18 17:55:59 -0800

    Do y2019 d9 p1

--- years/y2019/src/d09.rs
+++ years/y2019/src/d09.rs
@@ -1,14 +1,22 @@
-pub fn p1(s: &str) -> u32 {
-  todo!()
+use crate::intcode::{parse, Intcode};
+
+pub fn p1(s: &str) -> i64 {
+  let mut p = Intcode::new(parse(s));
+  p.input(1);
+  let mut output = Vec::with_capacity(1);
+  assert!(p.run(&mut output).is_done());
+  let out = output.pop().unwrap();
+  assert!(output.is_empty());
+  out
 }
 
-pub fn p2(s: &str) -> u32 {
+pub fn p2(s: &str) -> i64 {
   todo!()
 }
 
 #[test]
 fn t() {
   let inp = include_str!("input/d09.txt");
-  // assert_eq!(p1(inp), ___);
+  assert_eq!(p1(inp), 2204990589);
   // assert_eq!(p2(inp), ___);
 }
