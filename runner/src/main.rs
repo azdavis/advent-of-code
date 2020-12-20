3c0e37962f2a914573fdcb4ec5a8ee5dc9c88a26
Ariel Davis <ariel.z.davis@icloud.com>
2020-12-19 22:37:41 -0800

    Start y2020 d20

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,4 +1,4 @@
 fn main() {
-  let inp = include_str!("../../years/y2020/src/input/d19.txt");
-  println!("{}", y2020::d19::p2(&inp));
+  let inp = include_str!("../../years/y2020/src/input/d20.txt");
+  println!("{}", y2020::d20::p1(&inp));
 }
