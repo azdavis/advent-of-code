f9b6fc8f4ded172a7c45d521d0c7fbb3268c66ef
Ariel Davis <ariel.z.davis@icloud.com>
2020-12-21 17:57:17 -0800

    Do y2019 d10 p1

--- years/y2019/src/d10.rs
+++ years/y2019/src/d10.rs
@@ -1,14 +1,74 @@
-pub fn p1(s: &str) -> u32 {
-  todo!()
+use helpers::gcd::gcd;
+use std::collections::HashSet;
+use std::convert::TryInto as _;
+
+pub fn p1(s: &str) -> usize {
+  let asteroids: HashSet<Point> = s
+    .lines()
+    .enumerate()
+    .flat_map(|(y, line)| {
+      line.chars().enumerate().filter_map(move |(x, c)| match c {
+        '.' => None,
+        '#' => Some((to_i32(x), to_i32(y))),
+        _ => panic!("bad tile: {}", c),
+      })
+    })
+    .collect();
+  asteroids
+    .iter()
+    .map(|&a| {
+      asteroids
+        .iter()
+        .filter(|&&b| can_see(a, b, &asteroids))
+        .count()
+    })
+    .max()
+    .unwrap()
 }
 
 pub fn p2(s: &str) -> u32 {
   todo!()
 }
 
+type Point = (i32, i32);
+
+/// returns whether `a` has a line of sight to `b` based on `points`
+fn can_see(a: Point, b: Point, points: &HashSet<Point>) -> bool {
+  if a == b {
+    // special-case this
+    return false;
+  }
+  let (mut ax, mut ay) = a;
+  let (bx, by) = b;
+  let dx = bx - ax;
+  let dy = by - ay;
+  let g = to_i32(gcd(to_usize(dx.abs()), to_usize(dy.abs())));
+  let dx = dx / g;
+  let dy = dy / g;
+  loop {
+    ax += dx;
+    ay += dy;
+    let a = (ax, ay);
+    if a == b {
+      return true;
+    }
+    if points.contains(&a) {
+      return false;
+    }
+  }
+}
+
+fn to_i32(n: usize) -> i32 {
+  n.try_into().unwrap()
+}
+
+fn to_usize(n: i32) -> usize {
+  n.try_into().unwrap()
+}
+
 #[test]
 fn t() {
   let inp = include_str!("input/d10.txt");
-  // assert_eq!(p1(inp), ___);
+  assert_eq!(p1(inp), 344);
   // assert_eq!(p2(inp), ___);
 }
