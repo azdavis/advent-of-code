29b064370f3b82614311661120f152a79edddf07
Ariel Davis <ariel.z.davis@icloud.com>
2020-12-22 17:23:42 -0800

    Do y2020 d22

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,4 +1,4 @@
 fn main() {
   let inp = include_str!("../../years/y2020/src/input/d22.txt");
-  println!("{}", y2020::d22::p1(&inp));
+  println!("{}", y2020::d22::p2(&inp));
 }
--- years/y2020/src/d22.rs
+++ years/y2020/src/d22.rs
@@ -1,14 +1,130 @@
-pub fn p1(s: &str) -> u32 {
-  todo!()
+use std::cmp::Ordering;
+use std::collections::{HashSet, VecDeque};
+
+pub fn p1(s: &str) -> usize {
+  let (d1, d2) = parse(s);
+  score(regular(d1, d2))
 }
 
-pub fn p2(s: &str) -> u32 {
-  todo!()
+pub fn p2(s: &str) -> usize {
+  let (d1, d2) = parse(s);
+  score(recursive(d1, d2).1)
+}
+
+type Card = usize;
+type Deck = VecDeque<Card>;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Player {
+  P1,
+  P2,
+}
+
+fn regular(mut d1: Deck, mut d2: Deck) -> Deck {
+  loop {
+    match (d1.is_empty(), d2.is_empty()) {
+      (true, true) => panic!("both decks empty"),
+      (false, true) => return d1,
+      (true, false) => return d2,
+      (false, false) => {}
+    }
+    let c1 = d1.pop_front().unwrap();
+    let c2 = d2.pop_front().unwrap();
+    match c1.cmp(&c2) {
+      Ordering::Less => {
+        d2.push_back(c2);
+        d2.push_back(c1);
+      }
+      Ordering::Equal => panic!("equal cards: {}", c1),
+      Ordering::Greater => {
+        d1.push_back(c1);
+        d1.push_back(c2);
+      }
+    }
+  }
+}
+
+fn recursive(mut d1: Deck, mut d2: Deck) -> (Player, Deck) {
+  let mut prev = HashSet::new();
+  loop {
+    if !prev.insert((d1.clone(), d2.clone())) {
+      return (Player::P1, d1);
+    }
+    match (d1.is_empty(), d2.is_empty()) {
+      (true, true) => panic!("both decks empty"),
+      (false, true) => return (Player::P1, d1),
+      (true, false) => return (Player::P2, d2),
+      (false, false) => {}
+    }
+    let c1 = d1.pop_front().unwrap();
+    let c2 = d2.pop_front().unwrap();
+    let winner = if d1.len() >= c1 && d2.len() >= c2 {
+      let d1: VecDeque<_> = d1.iter().take(c1).copied().collect();
+      let d2: VecDeque<_> = d2.iter().take(c2).copied().collect();
+      recursive(d1, d2).0
+    } else {
+      match c1.cmp(&c2) {
+        Ordering::Less => Player::P2,
+        Ordering::Equal => panic!("equal cards: {}", c1),
+        Ordering::Greater => Player::P1,
+      }
+    };
+    match winner {
+      Player::P1 => {
+        d1.push_back(c1);
+        d1.push_back(c2);
+      }
+      Player::P2 => {
+        d2.push_back(c2);
+        d2.push_back(c1);
+      }
+    }
+  }
+}
+
+fn score(deck: Deck) -> usize {
+  deck
+    .into_iter()
+    .rev()
+    .enumerate()
+    .map(|(idx, c)| (idx + 1) * c)
+    .sum()
+}
+
+fn parse(s: &str) -> (Deck, Deck) {
+  let mut lines = s.lines();
+  assert_eq!(lines.next().unwrap(), "Player 1:");
+  let mut d1 = Deck::new();
+  loop {
+    let next = lines.next().unwrap();
+    if next.is_empty() {
+      break;
+    }
+    d1.push_back(next.parse().unwrap());
+  }
+  assert_eq!(lines.next().unwrap(), "Player 2:");
+  let d2: Deck = lines.map(|x| x.parse().unwrap()).collect();
+  (d1, d2)
 }
 
 #[test]
 fn t() {
   let inp = include_str!("input/d22.txt");
-  // assert_eq!(p1(inp), ___);
-  // assert_eq!(p2(inp), ___);
+  assert_eq!(p1(inp), 31957);
+  assert_eq!(p2(inp), 33212);
+}
+
+#[test]
+fn t_p2_infinite() {
+  recursive(Deck::from(vec![43, 19]), Deck::from(vec![2, 29, 14]));
+}
+
+#[test]
+fn t_p2() {
+  let (p, d) = recursive(
+    Deck::from(vec![9, 2, 6, 3, 1]),
+    Deck::from(vec![5, 8, 4, 7, 10]),
+  );
+  assert_eq!(p, Player::P2);
+  assert_eq!(d, Deck::from(vec![7, 5, 6, 2, 4, 1, 10, 8, 9, 3]));
 }
