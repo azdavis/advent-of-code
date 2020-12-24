b59e2ad9c056f0e69f3ca1982f52e43570183058
Ariel Davis <ariel.z.davis@icloud.com>
2020-12-23 17:04:57 -0800

    Do y2020 d23 p2

--- years/y2020/src/d23.rs
+++ years/y2020/src/d23.rs
@@ -1,40 +1,26 @@
+use std::collections::HashMap;
+
 pub fn p1(s: &str) -> String {
-  p1_help(s, 100)
+  go_p1(s, 100)
 }
 
 pub fn p2(s: &str) -> u64 {
-  let mut cups = parse(s);
-  let max_cup = *cups.iter().max().unwrap();
-  cups.extend((max_cup + 1)..=1_000_000);
-  go(&mut cups, 10_000_000);
-  let mut idx = cups.iter().position(|&c| c == 1).unwrap();
-  idx += 1;
-  if idx >= cups.len() {
-    idx = 0;
-  }
-  let n1 = u64::from(cups[idx]);
-  idx += 1;
-  if idx >= cups.len() {
-    idx = 0;
-  }
-  let n2 = u64::from(cups[idx]);
-  n1 * n2
+  let (mut cups, first) = mk_map_p2(s);
+  go(&mut cups, first, 10_000_000);
+  let n1 = cups[&1];
+  let n2 = cups[&n1];
+  u64::from(n1) * u64::from(n2)
 }
 
-fn p1_help(s: &str, rounds: usize) -> String {
-  let mut cups = parse(s);
-  go(&mut cups, rounds);
-  let mut idx = cups.iter().position(|&c| c == 1).unwrap();
+const MAX_ELEM_P2: u32 = 1_000_000;
+
+fn go_p1(s: &str, rounds: usize) -> String {
+  let (mut cups, first) = mk_map_p1(s);
+  go(&mut cups, first, rounds);
   let mut ret = String::with_capacity(cups.len() - 1);
-  loop {
-    idx += 1;
-    if idx >= cups.len() {
-      idx = 0;
-    }
-    if cups[idx] == 1 {
-      return ret;
-    }
-    let c = match cups[idx] {
+  let mut cur = cups[&1];
+  while cur != 1 {
+    let c = match cur {
       0 => '0',
       1 => '1',
       2 => '2',
@@ -48,41 +34,68 @@ fn p1_help(s: &str, rounds: usize) -> String {
       n => panic!("not a digit: {}", n),
     };
     ret.push(c);
+    cur = cups[&cur];
   }
+  ret
 }
 
-fn go(cups: &mut Vec<u32>, rounds: usize) {
+fn mk_map_p1(s: &str) -> (HashMap<u32, u32>, u32) {
+  let cups = parse(s);
+  let first = *cups.first().unwrap();
+  let last = *cups.last().unwrap();
+  let map: HashMap<_, _> = cups
+    .windows(2)
+    .map(|xs| (xs[0], xs[1]))
+    .chain(std::iter::once((last, first)))
+    .collect();
+  (map, first)
+}
+
+fn mk_map_p2(s: &str) -> (HashMap<u32, u32>, u32) {
+  let cups = parse(s);
+  let first = *cups.first().unwrap();
+  let last = *cups.last().unwrap();
+  let max = *cups.iter().max().unwrap();
+  let map: HashMap<_, _> = cups
+    .windows(2)
+    .map(|xs| (xs[0], xs[1]))
+    .chain(std::iter::once((last, max + 1)))
+    .chain((max + 1..MAX_ELEM_P2).map(|x| (x, x + 1)))
+    .chain(std::iter::once((MAX_ELEM_P2, first)))
+    .collect();
+  (map, first)
+}
+
+// take advantage of the fact that there are no duplicate elements to avoid both
+// indexing logic and linked lists, and just map each element to its successor.
+fn go(cups: &mut HashMap<u32, u32>, mut cur: u32, rounds: usize) {
   assert!(cups.len() >= 5);
-  let min_cup = *cups.iter().min().unwrap();
-  let max_cup = *cups.iter().max().unwrap();
-  let mut cur_idx = 0;
+  let min_cup = *cups.values().min().unwrap();
+  let max_cup = *cups.values().max().unwrap();
   for _ in 0..rounds {
-    let mut pick_up = Vec::with_capacity(3);
-    for _ in 0..3 {
-      let mut rm_idx = cur_idx + 1;
-      if rm_idx >= cups.len() {
-        cur_idx -= 1;
-        rm_idx = 0;
-      }
-      pick_up.push(cups.remove(rm_idx));
-    }
-    let mut dest_val = cups[cur_idx] - 1;
+    let pick_up: Vec<_> = (0..3)
+      .map(|_| {
+        let to_rm = cups[&cur];
+        let next = cups[&to_rm];
+        cups.insert(cur, next);
+        to_rm
+      })
+      .collect();
+    let mut dest = cur - 1;
     loop {
-      if dest_val < min_cup {
-        dest_val = max_cup;
+      if dest < min_cup {
+        dest = max_cup;
       }
-      if !pick_up.contains(&dest_val) {
+      if !pick_up.contains(&dest) {
         break;
       }
-      dest_val -= 1;
+      dest -= 1;
     }
-    let mut dest_idx = cups.iter().position(|&c| c == dest_val).unwrap() + 1;
-    if dest_idx >= cups.len() {
-      dest_idx = 0;
+    for n in pick_up.into_iter().rev() {
+      let next = cups.insert(dest, n).unwrap();
+      cups.insert(n, next);
     }
-    cups.splice(dest_idx..dest_idx, pick_up);
-    cur_idx += if dest_idx <= cur_idx { 4 } else { 1 };
-    cur_idx %= cups.len();
+    cur = cups[&cur];
   }
 }
 
@@ -97,16 +110,54 @@ fn parse(s: &str) -> Vec<u32> {
 fn t() {
   let inp = include_str!("input/d23.txt");
   assert_eq!(p1(inp), "89372645");
-  // assert_eq!(p2(inp), ___);
+  assert_eq!(p2(inp), 21273394210);
 }
 
-#[test]
-fn t_p1() {
-  assert_eq!(p1_help("389125467", 10), "92658374");
-  assert_eq!(p1_help("389125467", 100), "67384529");
-}
+#[cfg(test)]
+mod tests {
+  use super::{go_p1, mk_map_p1, mk_map_p2, p2, MAX_ELEM_P2};
+  use helpers::maplit::hashmap;
 
-#[test]
-fn t_p2() {
-  assert_eq!(p2("389125467"), 149245887792);
+  #[test]
+  fn t_p1() {
+    assert_eq!(go_p1("389125467", 10), "92658374");
+    assert_eq!(go_p1("389125467", 100), "67384529");
+  }
+
+  #[test]
+  fn t_p2() {
+    assert_eq!(p2("389125467"), 149245887792);
+  }
+
+  #[test]
+  fn t_mk_map_p1() {
+    let (map, fst) = mk_map_p1("45312");
+    let want = hashmap![
+      4 => 5,
+      5 => 3,
+      3 => 1,
+      1 => 2,
+      2 => 4,
+    ];
+    assert_eq!(map, want);
+    assert_eq!(fst, 4);
+  }
+
+  #[test]
+  fn t_mk_map_p2() {
+    let (map, fst) = mk_map_p2("3215674");
+    assert_eq!(map.len(), MAX_ELEM_P2 as usize);
+    assert_eq!(map[&3], 2);
+    assert_eq!(map[&2], 1);
+    assert_eq!(map[&1], 5);
+    assert_eq!(map[&5], 6);
+    assert_eq!(map[&6], 7);
+    assert_eq!(map[&7], 4);
+    assert_eq!(map[&4], 8);
+    for i in 8..MAX_ELEM_P2 {
+      assert_eq!(map[&i], i + 1);
+    }
+    assert_eq!(map[&MAX_ELEM_P2], 3);
+    assert_eq!(fst, 3);
+  }
 }
