5e3657539fa016d79b87ab1ee479d5ebdd8e89f6
Ariel Davis <ariel.z.davis@icloud.com>
2020-12-24 18:37:14 -0800

    Do y2019 d11

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,4 +1,4 @@
 fn main() {
   let inp = include_str!("../../years/y2019/src/input/d11.txt");
-  println!("{}", y2019::d11::p1(&inp));
+  println!("{}", y2019::d11::p2(&inp));
 }
--- years/y2019/src/d11.rs
+++ years/y2019/src/d11.rs
@@ -1,14 +1,82 @@
-pub fn p1(s: &str) -> u32 {
-  todo!()
+use crate::intcode::{Intcode, Res};
+use helpers::compass::Compass;
+use helpers::maplit::hashset;
+use helpers::point::Point;
+use std::collections::HashSet;
+
+pub fn p1(s: &str) -> usize {
+  go(s, &mut hashset![])
 }
 
-pub fn p2(s: &str) -> u32 {
-  todo!()
+pub fn p2(s: &str) -> String {
+  let mut white = hashset![Point::default()];
+  go(s, &mut white);
+  let min_x = white.iter().map(|p| p.x).min().unwrap();
+  let min_y = white.iter().map(|p| p.y).min().unwrap();
+  let max_x = white.iter().map(|p| p.x).max().unwrap();
+  let max_y = white.iter().map(|p| p.y).max().unwrap();
+  // too lazy to do with with_capacity calculation
+  let mut ret = String::new();
+  for y in (min_y..=max_y).rev() {
+    for x in min_x..=max_x {
+      let c = if white.contains(&Point::new(x, y)) {
+        '█'
+      } else {
+        ' '
+      };
+      ret.push(c);
+    }
+    // the | prevents deleting trailing whitespace
+    ret.push(' ');
+    ret.push('|');
+    ret.push('\n');
+  }
+  ret
+}
+
+fn go(s: &str, white: &mut HashSet<Point>) -> usize {
+  let mut p = Intcode::parse(s);
+  let mut did_paint = HashSet::<Point>::new();
+  let mut cur = Point::default();
+  let mut facing = Compass::North;
+  let mut output = Vec::with_capacity(2);
+  loop {
+    match p.run(&mut output) {
+      Res::Done => return did_paint.len(),
+      Res::NeedInput => {}
+    }
+    match output[..] {
+      [] => p.input(if white.contains(&cur) { 1 } else { 0 }),
+      [color, dir] => {
+        match color {
+          0 => {
+            white.remove(&cur);
+          }
+          1 => {
+            white.insert(cur);
+          }
+          _ => panic!("bad color: {}", color),
+        }
+        did_paint.insert(cur);
+        facing = match dir {
+          0 => facing.left(),
+          1 => facing.right(),
+          _ => panic!("bad dir: {}", dir),
+        };
+        let (dx, dy) = facing.dx_dy();
+        cur.x += dx;
+        cur.y += dy;
+        output.clear();
+      }
+      _ => panic!("bad output len: {}", output.len()),
+    }
+  }
 }
 
 #[test]
 fn t() {
   let inp = include_str!("input/d11.txt");
-  // assert_eq!(p1(inp), ___);
-  // assert_eq!(p2(inp), ___);
+  assert_eq!(p1(inp), 2160);
+  let out = include_str!("snapshots/d11p2.txt");
+  assert_eq!(p2(inp), out);
 }
