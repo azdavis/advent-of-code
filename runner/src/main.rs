529b5b7a975b1f9b267ae992a4b185415e7fdc9c
Ariel Davis <ariel.z.davis@icloud.com>
2021-01-01 14:21:59 -0800

    Start y2019 d13

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,4 +1,4 @@
 fn main() {
-  let inp = include_str!("../../years/y2020/src/input/d25.txt");
-  println!("{}", y2020::d25::p1(&inp));
+  let inp = include_str!("../../years/y2019/src/input/d13.txt");
+  println!("{}", y2019::d13::p1(&inp));
 }
---
+++ years/y2019/src/d13.rs
@@ -0,0 +1,14 @@
+pub fn p1(_: &str) -> u32 {
+  todo!()
+}
+
+pub fn p2(_: &str) -> u32 {
+  todo!()
+}
+
+#[test]
+fn t() {
+  let inp = include_str!("input/d13.txt");
+  // assert_eq!(p1(inp), ___);
+  // assert_eq!(p2(inp), ___);
+}
