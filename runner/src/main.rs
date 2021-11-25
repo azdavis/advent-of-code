e944b1484d6fe58815fdd6151c708be06c6d0615
Ariel Davis <ariel.z.davis@icloud.com>
2021-11-25 02:59:52 -0800

    Do d3

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-  let s = include_str!("../../years/y2018/src/input/d02.txt");
-  println!("p1: {}", y2018::d02::p1(s));
-  println!("p2: {}", y2018::d02::p2(s));
+  let s = include_str!("../../years/y2018/src/input/d03.txt");
+  println!("p1: {}", y2018::d03::p1(s));
+  println!("p2: {}", y2018::d03::p2(s));
 }
---
+++ years/y2018/src/d03.rs
@@ -0,0 +1,71 @@
+use helpers::{Lazy, Regex};
+
+type Vec2 = [usize; 2];
+
+struct Claim {
+  id: usize,
+  pos: Vec2,
+  dim: Vec2,
+}
+
+const DIM: usize = 1000;
+static RE: Lazy<Regex> =
+  Lazy::new(|| Regex::new(r"^#(\d+) @ (\d+),(\d+): (\d+)x(\d+)$").unwrap());
+
+fn parse(s: &str) -> Vec<Claim> {
+  s.lines()
+    .map(|line| {
+      let caps = RE.captures(line).unwrap();
+      Claim {
+        id: caps[1].parse().unwrap(),
+        pos: [caps[2].parse().unwrap(), caps[3].parse().unwrap()],
+        dim: [caps[4].parse().unwrap(), caps[5].parse().unwrap()],
+      }
+    })
+    .collect()
+}
+
+fn run(claims: &[Claim]) -> Vec<Vec<usize>> {
+  let mut grid = vec![vec![0usize; DIM]; DIM];
+  for cl in claims {
+    let [x, y] = cl.pos;
+    let [dx, dy] = cl.dim;
+    for row in grid.iter_mut().skip(y).take(dy) {
+      for n in row.iter_mut().skip(x).take(dx) {
+        *n += 1;
+      }
+    }
+  }
+  grid
+}
+
+pub fn p1(s: &str) -> usize {
+  let grid = run(&parse(s));
+  grid.into_iter().flatten().filter(|&n| n > 1).count()
+}
+
+pub fn p2(s: &str) -> usize {
+  let claims = parse(s);
+  let grid = run(&claims);
+  for cl in claims {
+    let [x, y] = cl.pos;
+    let [dx, dy] = cl.dim;
+    let all_1 = grid
+      .iter()
+      .skip(y)
+      .take(dy)
+      .flat_map(|row| row.iter().skip(x).take(dx))
+      .all(|&n| n == 1);
+    if all_1 {
+      return cl.id;
+    }
+  }
+  panic!("no solution")
+}
+
+#[test]
+fn t() {
+  let s = include_str!("input/d03.txt");
+  assert_eq!(p1(s), 101565);
+  assert_eq!(p2(s), 656);
+}
