b2a47ec553f5874872a7d31ae3cbaaa859fd0a18
Ariel Davis <ariel.z.davis@icloud.com>
2021-11-25 00:54:13 -0800

    Do p2

--- years/y2018/src/d01.rs
+++ years/y2018/src/d01.rs
@@ -1,14 +1,30 @@
-pub fn p1(s: &str) -> i32 {
-  s.lines().map(|line| line.parse::<i32>().unwrap()).sum()
+use helpers::HashSet;
+
+fn parse(s: &str) -> impl Iterator<Item = i32> + '_ {
+  s.lines().map(|line| line.parse::<i32>().unwrap())
 }
 
-pub fn p2(s: &str) -> usize {
-  s.len()
+pub fn p1(s: &str) -> i32 {
+  parse(s).sum()
+}
+
+pub fn p2(s: &str) -> i32 {
+  let ns: Vec<_> = parse(s).collect();
+  let mut cur = 0i32;
+  let mut past = HashSet::<i32>::default();
+  loop {
+    for &n in ns.iter() {
+      cur += n;
+      if !past.insert(cur) {
+        return cur;
+      }
+    }
+  }
 }
 
 #[test]
 fn t() {
   let s = include_str!("input/d01.txt");
   assert_eq!(p1(s), 516);
-  // assert_eq!(p2(s), ___);
+  assert_eq!(p2(s), 71892);
 }
