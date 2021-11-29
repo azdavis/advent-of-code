be3824e0c299deb59bca9b1903e3e61b3cb413f9
Ariel Davis <ariel.z.davis@icloud.com>
2021-11-29 00:56:25 -0800

    Do p2

--- years/y2018/src/d09.rs
+++ years/y2018/src/d09.rs
@@ -9,24 +9,67 @@ fn parse(s: &str) -> (usize, u32) {
   (caps[1].parse().unwrap(), caps[2].parse().unwrap())
 }
 
+struct CycleZipper {
+  front: Vec<u32>,
+  rear: Vec<u32>,
+}
+
+impl CycleZipper {
+  fn new() -> CycleZipper {
+    CycleZipper {
+      front: vec![0],
+      rear: vec![],
+    }
+  }
+
+  fn move_next(&mut self) {
+    if self.front.is_empty() {
+      self.rear.reverse();
+      std::mem::swap(&mut self.front, &mut self.rear);
+    }
+    let v = self.front.pop().unwrap();
+    self.rear.push(v);
+  }
+
+  fn move_prev(&mut self) {
+    if self.rear.is_empty() {
+      self.front.reverse();
+      std::mem::swap(&mut self.front, &mut self.rear);
+    }
+    let v = self.rear.pop().unwrap();
+    self.front.push(v);
+  }
+
+  fn push(&mut self, n: u32) {
+    self.front.push(n);
+  }
+
+  fn pop(&mut self) -> u32 {
+    if self.front.is_empty() {
+      self.rear.reverse();
+      std::mem::swap(&mut self.front, &mut self.rear);
+    }
+    self.front.pop().unwrap()
+  }
+}
+
 fn run(n_players: usize, last: u32) -> u32 {
-  let mut marbles = vec![0u32];
+  let mut marbles = CycleZipper::new();
   let mut scores = vec![0u32; n_players];
   let mut next_marble = 1u32;
-  let mut cur_idx = 0usize;
   let mut cur_player = 0usize;
   loop {
     if next_marble % 23 == 0 {
-      while cur_idx < 7 {
-        cur_idx += marbles.len();
+      for _ in 0..7 {
+        marbles.move_prev();
       }
-      cur_idx -= 7;
-      let rm = marbles.remove(cur_idx);
+      let rm = marbles.pop();
       scores[cur_player] += rm + next_marble;
     } else {
-      cur_idx += 2;
-      cur_idx %= marbles.len();
-      marbles.insert(cur_idx, next_marble);
+      for _ in 0..2 {
+        marbles.move_next();
+      }
+      marbles.push(next_marble);
     }
     if next_marble == last {
       return scores.iter().copied().max().unwrap();
@@ -51,7 +94,7 @@ pub fn p2(s: &str) -> u32 {
 fn t() {
   let s = include_str!("input/d09.txt");
   assert_eq!(p1(s), 399645);
-  // assert_eq!(p2(s), 0);
+  assert_eq!(p2(s), 3352507536);
 }
 
 #[cfg(test)]
