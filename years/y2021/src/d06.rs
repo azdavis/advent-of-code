bb7f81d083a928cdcac7d555e9cf5475dd54231a
Ariel Davis <ariel.z.davis@icloud.com>
2021-12-06 02:31:52 -0500

    Do 2021 day 6

---
+++ years/y2021/src/d06.rs
@@ -0,0 +1,39 @@
+const RESTART: usize = 6;
+const MAX: usize = RESTART + 2;
+
+fn run(s: &str, rounds: usize) -> usize {
+  let mut fish = vec![0usize; MAX + 1];
+  for idx in s.trim().split(',') {
+    let idx: usize = idx.parse().unwrap();
+    fish[idx] += 1;
+  }
+  for _ in 0..rounds {
+    fish = (0usize..=MAX)
+      .map(|idx| {
+        if idx == RESTART {
+          fish[0] + fish[RESTART + 1]
+        } else if idx == MAX {
+          fish[0]
+        } else {
+          fish[idx + 1]
+        }
+      })
+      .collect();
+  }
+  fish.into_iter().sum()
+}
+
+pub fn p1(s: &str) -> usize {
+  run(s, 80)
+}
+
+pub fn p2(s: &str) -> usize {
+  run(s, 256)
+}
+
+#[test]
+fn t() {
+  let s = include_str!("input/d06.txt");
+  assert_eq!(p1(s), 371379);
+  assert_eq!(p2(s), 1674303997472);
+}
--- years/y2021/src/lib.rs
+++ years/y2021/src/lib.rs
@@ -6,3 +6,5 @@ pub mod d03;
 pub mod d04;
 
 pub mod d05;
+
+pub mod d06;
