021a2e178b282b3e213856fa9b6c423a2739d9ae
Ariel Davis <ariel.z.davis@icloud.com>
2021-12-06 02:08:39 -0500

    Do 2015 day 2

---
+++ years/y2015/src/d02.rs
@@ -0,0 +1,41 @@
+type Dims = [u32; 3];
+
+fn parse(s: &str) -> impl Iterator<Item = Dims> + '_ {
+  s.lines().map(|line| {
+    let mut parts = line.split('x');
+    let l: u32 = parts.next().unwrap().parse().unwrap();
+    let w: u32 = parts.next().unwrap().parse().unwrap();
+    let h: u32 = parts.next().unwrap().parse().unwrap();
+    assert!(parts.next().is_none());
+    [l, w, h]
+  })
+}
+
+pub fn p1(s: &str) -> u32 {
+  parse(s)
+    .map(|[l, w, h]| {
+      let a = l * w;
+      let b = w * h;
+      let c = h * l;
+      (a).min(b).min(c) + (2 * (a + b + c))
+    })
+    .sum()
+}
+
+pub fn p2(s: &str) -> u32 {
+  parse(s)
+    .map(|[l, w, h]| {
+      let a = 2 * (l + w);
+      let b = 2 * (w + h);
+      let c = 2 * (h + l);
+      a.min(b).min(c) + (l * w * h)
+    })
+    .sum()
+}
+
+#[test]
+fn t() {
+  let s = include_str!("input/d02.txt");
+  assert_eq!(p1(s), 1586300);
+  assert_eq!(p2(s), 3737498);
+}
