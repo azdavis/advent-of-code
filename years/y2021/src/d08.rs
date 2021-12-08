3494a6f1c477b2fb6f972aa131d7db0a15f9b7ae
Ariel Davis <ariel.z.davis@icloud.com>
2021-12-08 03:30:06 -0500

    Rename

--- years/y2021/src/d08.rs
+++ years/y2021/src/d08.rs
@@ -18,13 +18,13 @@ fn get_count(counts: &Counts, want: u8) -> impl Iterator<Item = char> + '_ {
     .filter_map(move |(&c, &count)| (count == want).then(|| c))
 }
 
-fn get_digit<F>(digits: &[&str], len: usize, pred: F) -> char
+fn get_digit<F>(all_digits: &[&str], len: usize, pred: F) -> char
 where
   F: FnMut(&char) -> bool,
 {
-  digits
+  all_digits
     .iter()
-    .find(|&&def| def.len() == len)
+    .find(|&&digit| digit.len() == len)
     .unwrap()
     .chars()
     .find(pred)
@@ -32,9 +32,9 @@ where
 }
 
 fn get_one_line(line: &str) -> usize {
-  let (digits, num) = line.split_once(" | ").unwrap();
+  let (all_digits, num) = line.split_once(" | ").unwrap();
   let mut counts: Counts = ('a'..='g').map(|c| (c, 0u8)).collect();
-  for c in digits.chars() {
+  for c in all_digits.chars() {
     if let Some(count) = counts.get_mut(&c) {
       *count += 1;
     }
@@ -42,13 +42,13 @@ fn get_one_line(line: &str) -> usize {
   let seg_e = get_count(&counts, 4).next().unwrap();
   let seg_b = get_count(&counts, 6).next().unwrap();
   let seg_f = get_count(&counts, 9).next().unwrap();
-  let digits: Vec<_> = digits.split(' ').collect();
-  let seg_c = get_digit(&digits, 2, |&c| c != seg_f);
+  let all_digits: Vec<_> = all_digits.split(' ').collect();
+  let seg_c = get_digit(&all_digits, 2, |&c| c != seg_f);
   let seg_a = get_count(&counts, 8).find(|&c| c != seg_c).unwrap();
   let seg_d =
-    get_digit(&digits, 4, |&c| c != seg_b && c != seg_c && c != seg_f);
+    get_digit(&all_digits, 4, |&c| c != seg_b && c != seg_c && c != seg_f);
   let seg_g = get_count(&counts, 7).find(|&c| c != seg_d).unwrap();
-  let mapping = [
+  let digit_mapping = [
     hash_set([seg_a, seg_b, seg_c, seg_e, seg_f, seg_g]),
     hash_set([seg_c, seg_f]),
     hash_set([seg_a, seg_c, seg_d, seg_e, seg_g]),
@@ -62,12 +62,12 @@ fn get_one_line(line: &str) -> usize {
   ];
   num
     .split(' ')
-    .map(|digit| {
-      let digits: HashSet<_> = digit.chars().collect();
-      mapping
+    .map(|num_digit| {
+      let num_digit: HashSet<_> = num_digit.chars().collect();
+      digit_mapping
         .iter()
         .enumerate()
-        .find_map(|(idx, ds)| (digits == *ds).then(|| idx))
+        .find_map(|(idx, ds)| (num_digit == *ds).then(|| idx))
         .unwrap()
     })
     .fold(0usize, |ac, x| (ac * 10) + x)
