46d618687916c2dd634b382dd75c85152c33a6b8
Ariel Davis <ariel.z.davis@icloud.com>
2021-12-10 00:55:25 -0500

    Do 2015 day 8

---
+++ years/y2015/src/d08.rs
@@ -0,0 +1,61 @@
+fn decode_len(s: &str) -> usize {
+  let mut iter = s.chars();
+  let mut ret = 0usize;
+  assert_eq!(iter.next().unwrap(), '"');
+  loop {
+    match iter.next().unwrap() {
+      '"' => {
+        assert!(iter.next().is_none());
+        return ret;
+      }
+      '\\' => match iter.next().unwrap() {
+        '"' | '\\' => {}
+        'x' => {
+          assert!(iter.next().unwrap().is_digit(16));
+          assert!(iter.next().unwrap().is_digit(16));
+        }
+        c => panic!("unknown escape: {}", c),
+      },
+      _ => {}
+    }
+    ret += 1;
+  }
+}
+
+pub fn p1(s: &str) -> usize {
+  let mut code = 0usize;
+  let mut decode = 0usize;
+  for line in s.lines() {
+    code += line.len();
+    decode += decode_len(line);
+  }
+  code - decode
+}
+
+fn encode_len(s: &str) -> usize {
+  let sum: usize = s
+    .chars()
+    .map(|c| match c {
+      '"' | '\\' => 2,
+      _ => 1,
+    })
+    .sum();
+  sum + 2
+}
+
+pub fn p2(s: &str) -> usize {
+  let mut code = 0usize;
+  let mut encode = 0usize;
+  for line in s.lines() {
+    code += line.len();
+    encode += encode_len(line);
+  }
+  encode - code
+}
+
+#[test]
+fn t() {
+  let s = include_str!("input/d08.txt");
+  assert_eq!(p1(s), 1350);
+  assert_eq!(p2(s), 2085);
+}
