aa3ef5c03b6326703009bcad12a383a4e72930a9
Ariel Davis <ariel.z.davis@icloud.com>
2021-12-10 18:28:49 -0800

    Do 2015 day 12

---
+++ years/y2015/src/d12.rs
@@ -0,0 +1,35 @@
+use serde_json::{from_str, Value};
+
+fn parse(s: &str) -> Value {
+  from_str(s.trim()).unwrap()
+}
+
+fn sum_val(v: &Value, p2: bool) -> i64 {
+  match v {
+    Value::Null | Value::Bool(_) | Value::String(_) => 0,
+    Value::Number(n) => n.as_i64().unwrap(),
+    Value::Array(vs) => vs.iter().map(|v| sum_val(v, p2)).sum(),
+    Value::Object(obj) => {
+      if p2 && obj.values().any(|x| x.as_str() == Some("red")) {
+        0
+      } else {
+        obj.values().map(|v| sum_val(v, p2)).sum()
+      }
+    }
+  }
+}
+
+pub fn p1(s: &str) -> i64 {
+  sum_val(&parse(s), false)
+}
+
+pub fn p2(s: &str) -> i64 {
+  sum_val(&parse(s), true)
+}
+
+#[test]
+fn t() {
+  let s = include_str!("input/d12.txt");
+  assert_eq!(p1(s), 156366);
+  assert_eq!(p2(s), 96852);
+}
--- years/y2015/src/lib.rs
+++ years/y2015/src/lib.rs
@@ -11,3 +11,4 @@ pub mod d08;
 pub mod d09;
 pub mod d10;
 pub mod d11;
+pub mod d12;
