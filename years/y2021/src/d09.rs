2d5e9830244c977c262351215fa385bac753d401
Ariel Davis <ariel.z.davis@icloud.com>
2021-12-12 02:03:09 -0800

    Do p2

--- years/y2021/src/d09.rs
+++ years/y2021/src/d09.rs
@@ -1,15 +1,21 @@
-use helpers::matrix::neighbors;
+use helpers::matrix::{neighbors, Coord};
+use helpers::HashSet;
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
 
-pub fn p1(s: &str) -> u32 {
-  let grid: Vec<Vec<u8>> = s
-    .lines()
+fn parse(s: &str) -> Vec<Vec<u8>> {
+  s.lines()
     .map(|line| {
       line
         .chars()
         .map(|c| c.to_digit(10).unwrap().try_into().unwrap())
         .collect()
     })
-    .collect();
+    .collect()
+}
+
+pub fn p1(s: &str) -> u32 {
+  let grid = parse(s);
   grid
     .iter()
     .enumerate()
@@ -27,14 +33,41 @@ pub fn p1(s: &str) -> u32 {
     .sum()
 }
 
+const TOP_SIZES: usize = 3;
+const MAX: u8 = 9;
+
 pub fn p2(s: &str) -> usize {
-  // TODO
-  s.len()
+  let grid = parse(s);
+  let mut visited = HashSet::<Coord>::default();
+  let mut sizes = BinaryHeap::<Reverse<usize>>::default();
+  for (y, row) in grid.iter().enumerate() {
+    for (x, &num) in row.iter().enumerate() {
+      if visited.contains(&[x, y]) || num == MAX {
+        continue;
+      }
+      let mut size = 0usize;
+      let mut stack = vec![[x, y]];
+      while let Some([x, y]) = stack.pop() {
+        if visited.contains(&[x, y]) || grid[y][x] == MAX {
+          continue;
+        }
+        visited.insert([x, y]);
+        size += 1;
+        stack.extend(neighbors(&grid, [x, y]).map(|it| it.1));
+      }
+      sizes.push(Reverse(size));
+      if sizes.len() > TOP_SIZES {
+        sizes.pop();
+      }
+    }
+  }
+  assert_eq!(sizes.len(), TOP_SIZES);
+  sizes.into_iter().map(|it| it.0).product()
 }
 
 #[test]
 fn t() {
   let s = include_str!("input/d09.txt");
   assert_eq!(p1(s), 539);
-  // assert_eq!(p2(s), 0);
+  assert_eq!(p2(s), 736920);
 }
