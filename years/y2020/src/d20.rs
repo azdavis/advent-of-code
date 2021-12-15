3c74be860870dc27f89aec73bc8d619c9bfa30b0
Ariel Davis <ariel.z.davis@icloud.com>
2021-12-14 23:18:45 -0800

    Move matrix helpers out of helpers

--- years/y2020/src/d20.rs
+++ years/y2020/src/d20.rs
@@ -1,6 +1,97 @@
-use helpers::matrix::{bot, left, right, rotate_left, top, transpose};
 use helpers::{static_regex, HashMap, HashSet};
 use std::cmp::Ordering;
+
+/// Rotates the matrix to the left. Requires there exists c such that for all x
+/// in xs, x.len() == c.
+fn rotate_left<T>(xs: &[Vec<T>]) -> Vec<Vec<T>>
+where
+  T: Copy,
+{
+  (0..xs.first().map_or(0, Vec::len))
+    .map(|j| xs.iter().rev().map(|row| row[j]).collect())
+    .collect()
+}
+
+#[test]
+fn t_rotate_left() {
+  let e: Vec<Vec<u32>> = vec![];
+  let es: &[Vec<u32>] = &[];
+  assert_eq!(rotate_left(es), e);
+  assert_eq!(rotate_left(&[vec![1]]), [[1]]);
+  assert_eq!(rotate_left(&[vec![1, 2], vec![3, 4]]), [[3, 1], [4, 2]]);
+  assert_eq!(rotate_left(&[vec![1, 2]]), [[1], [2]]);
+  assert_eq!(rotate_left(&[vec![1], vec![2]]), [[2, 1]]);
+  assert_eq!(
+    rotate_left(&[vec![1, 2, 3], vec![4, 5, 6]]),
+    [[4, 1], [5, 2], [6, 3]]
+  );
+  assert_eq!(
+    rotate_left(&[vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]]),
+    [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
+  );
+}
+
+/// Transposes the matrix. Requires there exists c such that for all x in xs,
+/// x.len() == c.
+fn transpose<T>(xs: &[Vec<T>]) -> Vec<Vec<T>>
+where
+  T: Copy,
+{
+  (0..xs.first().map_or(0, Vec::len))
+    .map(|j| xs.iter().map(|row| row[j]).collect())
+    .collect()
+}
+
+#[test]
+fn t_transpose() {
+  let e: Vec<Vec<u32>> = vec![];
+  let es: &[Vec<u32>] = &[];
+  assert_eq!(transpose(es), e);
+  assert_eq!(transpose(&[vec![1]]), [[1]]);
+  assert_eq!(transpose(&[vec![1, 2], vec![3, 4]]), [[1, 3], [2, 4]]);
+  assert_eq!(transpose(&[vec![1, 2]]), [[1], [2]]);
+  assert_eq!(transpose(&[vec![1], vec![2]]), [[1, 2]]);
+  assert_eq!(
+    transpose(&[vec![1, 2, 3], vec![4, 5, 6]]),
+    [[1, 4], [2, 5], [3, 6]]
+  );
+  assert_eq!(
+    transpose(&[vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]]),
+    [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+  );
+}
+
+/// Gets the topmost row.
+fn top<T>(xs: &[Vec<T>]) -> Vec<T>
+where
+  T: Copy,
+{
+  xs.first().unwrap().clone()
+}
+
+/// Gets the bottommost row.
+fn bot<T>(xs: &[Vec<T>]) -> Vec<T>
+where
+  T: Copy,
+{
+  xs.last().unwrap().clone()
+}
+
+/// Gets the leftmost column.
+fn left<T>(xs: &[Vec<T>]) -> Vec<T>
+where
+  T: Copy,
+{
+  xs.iter().map(|x| *x.first().unwrap()).collect()
+}
+
+/// Gets the rightmost column.
+fn right<T>(xs: &[Vec<T>]) -> Vec<T>
+where
+  T: Copy,
+{
+  xs.iter().map(|x| *x.last().unwrap()).collect()
+}
 
 pub fn p1(s: &str) -> u64 {
   let board = go(s);
