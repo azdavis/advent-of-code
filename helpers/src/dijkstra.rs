caa78206616cc0b81ce603bdd58c1a4c235932f9
Ariel Davis <ariel.z.davis@icloud.com>
2021-12-15 00:00:53 -0800

    Clean up

--- helpers/src/dijkstra.rs
+++ helpers/src/dijkstra.rs
@@ -1,7 +1,7 @@
 //! Dijkstra's algorithm for graphs.
 
 use crate::infinitable::Infinitable;
-use crate::{HashMap, HashSet};
+use crate::{hash_map, HashMap, HashSet};
 use std::cmp::Reverse;
 use std::collections::BinaryHeap;
 use std::hash::Hash;
@@ -17,10 +17,10 @@ pub trait Graph {
   /// Returns the neighbors of `node` in the graph.
   fn neighbors(&self, node: Self::Node) -> HashSet<Self::Node>;
 
-  /// Returns the distance between the nodes in the graph.
+  /// Returns the distance from `_a` to `_b` in the graph.
   ///
   /// The default implementation always returns 1.
-  fn distance(&self, _: Self::Node, _: Self::Node) -> usize {
+  fn distance(&self, _a: Self::Node, _b: Self::Node) -> usize {
     1
   }
 }
@@ -33,17 +33,10 @@ where
   G::Node: Hash + Ord + Copy,
 {
   // Don't need to store predecessors info (cf wikipedia).
-  let mut distances = HashMap::default();
-  distances.insert(start, Infinitable::Finite(0));
-  let mut pq: BinaryHeap<_> = distances
-    .iter()
-    .map(|(&node, &dist)| Elem::new(node, dist))
-    .collect();
+  let mut dist = hash_map([(start, Infinitable::Finite(0))]);
+  let mut pq = BinaryHeap::from([Elem::new(start, Infinitable::Finite(0))]);
   while let Some(u) = pq.pop() {
-    let u_dist = distances
-      .get(&u.node)
-      .copied()
-      .unwrap_or(Infinitable::PosInf);
+    let u_dist = dist.get(&u.node).copied().unwrap_or(Infinitable::PosInf);
     if u.node == end {
       match u_dist {
         Infinitable::Finite(x) => return Some(x),
@@ -54,13 +47,12 @@ where
       continue;
     }
     for node in graph.neighbors(u.node) {
-      let old_dist =
-        distances.get(&node).copied().unwrap_or(Infinitable::PosInf);
+      let old_dist = dist.get(&node).copied().unwrap_or(Infinitable::PosInf);
       let new_dist = u_dist + graph.distance(u.node, node);
       if new_dist >= old_dist {
         continue;
       }
-      distances.insert(node, new_dist);
+      dist.insert(node, new_dist);
       pq.push(Elem::new(node, new_dist));
     }
   }
