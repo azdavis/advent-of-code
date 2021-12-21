3894e9e33c4b80eb33507316c99033de392f333c
Ariel Davis <ariel.z.davis@icloud.com>
2021-12-20 19:56:35 -0800

    Do 2017 day 2

---
+++ years/y2017/src/d02.rs
@@ -0,0 +1,35 @@
+fn run(s: &str, f: fn(Vec<u32>) -> u32) -> u32 {
+  s.lines()
+    .map(|line| {
+      let ns: Vec<u32> = line
+        .split_ascii_whitespace()
+        .map(|it| it.parse().unwrap())
+        .collect();
+      f(ns)
+    })
+    .sum()
+}
+
+pub fn p1(s: &str) -> u32 {
+  run(s, |ns| ns.iter().max().unwrap() - ns.iter().min().unwrap())
+}
+
+pub fn p2(s: &str) -> u32 {
+  run(s, |ns| {
+    for a in ns.iter() {
+      for b in ns.iter() {
+        if a != b && (a % b == 0 || b % a == 0) {
+          return a.max(b) / a.min(b);
+        }
+      }
+    }
+    panic!("no solution for {:?}", ns)
+  })
+}
+
+#[test]
+fn t() {
+  let s = include_str!("input/d02.txt");
+  assert_eq!(p1(s), 53460);
+  assert_eq!(p2(s), 282);
+}
--- years/y2017/src/lib.rs
+++ years/y2017/src/lib.rs
@@ -1,3 +1,4 @@
 #![deny(rust_2018_idioms)]
 
 pub mod d01;
+pub mod d02;
