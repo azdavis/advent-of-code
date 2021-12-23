6326a359764165de90bdf0d2540554e9275fa947
Ariel Davis <ariel.z.davis@icloud.com>
2021-12-23 01:47:12 -0800

    Do 2021 day 19

---
+++ years/y2021/src/d19.rs
@@ -0,0 +1,155 @@
+use helpers::{HashMap, HashSet};
+
+type Coord = [i32; 3];
+type Scanner = HashSet<Coord>;
+
+fn parse(s: &str) -> HashMap<u8, Scanner> {
+  let mut lines = s.lines();
+  let mut ret = HashMap::<u8, Scanner>::default();
+  let mut idx = 0u8;
+  while lines.next().is_some() {
+    let mut scanner = Scanner::default();
+    for line in lines.by_ref() {
+      if line.is_empty() {
+        break;
+      }
+      let mut parts = line.split(',');
+      let x: i32 = parts.next().unwrap().parse().unwrap();
+      let y: i32 = parts.next().unwrap().parse().unwrap();
+      let z: i32 = parts.next().unwrap().parse().unwrap();
+      scanner.insert([x, y, z]);
+    }
+    ret.insert(idx, scanner);
+    idx += 1;
+  }
+  ret
+}
+
+fn all_translations(sc: &Scanner) -> [Scanner; 24] {
+  let mut ret = vec![Scanner::default(); 24];
+  for &coord in sc {
+    for (set, coord) in ret.iter_mut().zip(all_translations_coord(coord)) {
+      set.insert(coord);
+    }
+  }
+  ret.try_into().unwrap()
+}
+
+fn all_translations_coord(coord: Coord) -> [Coord; 24] {
+  let [x, y, z] = coord;
+  [
+    [x, y, z],
+    [x, z, -y],
+    [x, -y, -z],
+    [x, -z, y],
+    [-x, y, -z],
+    [-x, -z, -y],
+    [-x, -y, z],
+    [-x, z, y],
+    [y, -x, z],
+    [y, z, x],
+    [y, x, -z],
+    [y, -z, -x],
+    [-y, x, z],
+    [-y, z, -x],
+    [-y, -x, -z],
+    [-y, -z, x],
+    [z, y, -x],
+    [z, -x, -y],
+    [z, -y, x],
+    [z, x, y],
+    [-z, y, x],
+    [-z, x, -y],
+    [-z, -y, -x],
+    [-z, -x, y],
+  ]
+}
+
+fn align(sc: &Scanner, a: Coord, b: Coord) -> (Coord, Scanner) {
+  assert!(sc.contains(&a));
+  let [ax, ay, az] = a;
+  let [bx, by, bz] = b;
+  let dx = bx - ax;
+  let dy = by - ay;
+  let dz = bz - az;
+  let ret_sc: Scanner = sc
+    .iter()
+    .map(|&[x, y, z]| [x + dx, y + dy, z + dz])
+    .collect();
+  ([dx, dy, dz], ret_sc)
+}
+
+const THRESHOLD: usize = 12;
+
+fn run(s: &str) -> Vec<(Coord, Scanner)> {
+  let mut scanners = parse(s);
+  let mut ret: Vec<(Coord, Scanner)> =
+    vec![([0, 0, 0], scanners.remove(&0).unwrap())];
+  // slow AF.
+  while !scanners.is_empty() {
+    let (idx, offset, sc) = scanners
+      .iter()
+      .find_map(|(&idx, sc)| {
+        all_translations(sc).into_iter().find_map(|tr_sc| {
+          ret.iter().find_map(|(_, kn_sc)| {
+            kn_sc.iter().find_map(|&kn_sc_pt| {
+              tr_sc.iter().find_map(|&tr_sc_pt| {
+                let (offset, new_tr_sc) = align(&tr_sc, tr_sc_pt, kn_sc_pt);
+                let overlaps = kn_sc.intersection(&new_tr_sc).count();
+                (overlaps >= THRESHOLD).then(|| (idx, offset, new_tr_sc))
+              })
+            })
+          })
+        })
+      })
+      .unwrap();
+    scanners.remove(&idx);
+    ret.push((offset, sc));
+  }
+  ret
+}
+
+fn get_p1(res: &[(Coord, Scanner)]) -> usize {
+  let set: HashSet<_> = res.iter().flat_map(|(_, sc)| sc).collect();
+  set.len()
+}
+
+pub fn p1(s: &str) -> usize {
+  get_p1(&run(s))
+}
+
+fn city_dist(a: Coord, b: Coord) -> i32 {
+  let [ax, ay, az] = a;
+  let [bx, by, bz] = b;
+  (ax - bx).abs() + (ay - by).abs() + (az - bz).abs()
+}
+
+fn get_p2(res: &[(Coord, Scanner)]) -> i32 {
+  let offsets: Vec<_> = res.iter().map(|&(off, _)| off).collect();
+  offsets
+    .iter()
+    .flat_map(|&a| offsets.iter().map(move |&b| city_dist(a, b)))
+    .max()
+    .unwrap()
+}
+
+pub fn p2(s: &str) -> i32 {
+  get_p2(&run(s))
+}
+
+#[test]
+fn t() {
+  let s = include_str!("input/d19.txt");
+  // speed up the tests ~2x
+  let res = run(s);
+  assert_eq!(get_p1(&res), 396);
+  assert_eq!(get_p2(&res), 11828);
+}
+
+#[test]
+fn ex2() {
+  let s = include_str!("input/d19_ex2.txt");
+  let res = run(s);
+  assert_eq!(get_p1(&res), 79);
+  assert_eq!(get_p2(&res), 3621);
+}
--- years/y2021/src/lib.rs
+++ years/y2021/src/lib.rs
@@ -18,3 +18,4 @@ pub mod d15;
 pub mod d16;
 pub mod d17;
 pub mod d18;
+pub mod d19;
