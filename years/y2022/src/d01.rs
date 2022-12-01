57a50b4e2d4b9e65ba13e80dbf6c85d0ecf23f3b
Ariel Davis <ariel.z.davis@icloud.com>
2022-12-01 01:22:32 -0800

    Start y2022

---
+++ years/y2022/src/d01.rs
@@ -0,0 +1,30 @@
+pub fn p1(s: &str) -> u32 {
+  get_all(s).into_iter().max().unwrap()
+}
+
+pub fn p2(s: &str) -> u32 {
+  let mut all = get_all(s);
+  all.sort_unstable();
+  all.into_iter().rev().take(3).sum()
+}
+
+fn get_all(s: &str) -> Vec<u32> {
+  let mut cur = 0u32;
+  let mut all = Vec::<u32>::new();
+  for line in s.lines() {
+    if line.is_empty() {
+      all.push(cur);
+      cur = 0;
+    } else {
+      cur += line.parse::<u32>().unwrap();
+    }
+  }
+  all
+}
+
+#[test]
+fn t() {
+  let s = include_str!("input/d01.txt");
+  assert_eq!(p1(s), 66487);
+  assert_eq!(p2(s), 197301);
+}
