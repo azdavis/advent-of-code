553725638de75d19d60e282b2820dc3bcba16f83
Ariel Davis <ariel.z.davis@icloud.com>
2022-12-02 04:25:34 -0800

    Refactor

--- years/y2022/src/d02.rs
+++ years/y2022/src/d02.rs
@@ -5,43 +5,90 @@ enum Hand {
   Scissors,
 }
 
-pub fn p1(s: &str) -> u32 {
+impl Hand {
+  fn score(self) -> u32 {
+    match self {
+      Hand::Rock => 1,
+      Hand::Paper => 2,
+      Hand::Scissors => 3,
+    }
+  }
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Outcome {
+  Lose,
+  Tie,
+  Win,
+}
+
+impl Outcome {
+  fn score(self) -> u32 {
+    match self {
+      Outcome::Lose => 0,
+      Outcome::Tie => 3,
+      Outcome::Win => 6,
+    }
+  }
+}
+
+enum SecondColumn {
+  X,
+  Y,
+  Z,
+}
+
+fn parse_line(line: &str) -> (Hand, SecondColumn) {
+  let mut chars = line.chars();
+  let fst = match chars.next().unwrap() {
+    'A' => Hand::Rock,
+    'B' => Hand::Paper,
+    'C' => Hand::Scissors,
+    c => panic!("unknown first col char: {c}"),
+  };
+  assert_eq!(chars.next().unwrap(), ' ');
+  let snd = match chars.next().unwrap() {
+    'X' => SecondColumn::X,
+    'Y' => SecondColumn::Y,
+    'Z' => SecondColumn::Z,
+    c => panic!("unknown second col char: {c}"),
+  };
+  assert!(chars.next().is_none());
+  (fst, snd)
+}
+
+fn get<F>(s: &str, mut f: F) -> u32
+where
+  F: FnMut(Hand, SecondColumn) -> u32,
+{
   s.lines()
     .map(|line| {
-      let mut chars = line.chars();
-      let opponent = match chars.next().unwrap() {
-        'A' => Hand::Rock,
-        'B' => Hand::Paper,
-        'C' => Hand::Scissors,
-        c => panic!("unknown first col char: {c}"),
-      };
-      assert_eq!(chars.next().unwrap(), ' ');
-      let me = match chars.next().unwrap() {
-        'X' => Hand::Rock,
-        'Y' => Hand::Paper,
-        'Z' => Hand::Scissors,
-        c => panic!("unknown second col char: {c}"),
-      };
-      assert!(chars.next().is_none());
-      let shape_score: u32 = match me {
-        Hand::Rock => 1,
-        Hand::Paper => 2,
-        Hand::Scissors => 3,
-      };
-      let outcome_score: u32 = match (me, opponent) {
-        (Hand::Rock, Hand::Rock)
-        | (Hand::Paper, Hand::Paper)
-        | (Hand::Scissors, Hand::Scissors) => 3,
-        (Hand::Rock, Hand::Scissors)
-        | (Hand::Paper, Hand::Rock)
-        | (Hand::Scissors, Hand::Paper) => 6,
-        (Hand::Scissors, Hand::Rock)
-        | (Hand::Rock, Hand::Paper)
-        | (Hand::Paper, Hand::Scissors) => 0,
-      };
-      shape_score + outcome_score
+      let (opponent, snd) = parse_line(line);
+      f(opponent, snd)
     })
     .sum()
+}
+
+pub fn p1(s: &str) -> u32 {
+  get(s, |opponent, snd| {
+    let me = match snd {
+      SecondColumn::X => Hand::Rock,
+      SecondColumn::Y => Hand::Paper,
+      SecondColumn::Z => Hand::Scissors,
+    };
+    let outcome = match (me, opponent) {
+      (Hand::Rock, Hand::Rock)
+      | (Hand::Paper, Hand::Paper)
+      | (Hand::Scissors, Hand::Scissors) => Outcome::Tie,
+      (Hand::Rock, Hand::Scissors)
+      | (Hand::Paper, Hand::Rock)
+      | (Hand::Scissors, Hand::Paper) => Outcome::Win,
+      (Hand::Scissors, Hand::Rock)
+      | (Hand::Rock, Hand::Paper)
+      | (Hand::Paper, Hand::Scissors) => Outcome::Lose,
+    };
+    me.score() + outcome.score()
+  })
 }
 
 pub fn p2(s: &str) -> usize {
