4206611f3518b22d882664603a29b6ed6bf24548
Ariel Davis <ariel.z.davis@icloud.com>
2023-03-12 17:07:56 -0700

    Do 7a

---
+++ years/y2022/src/d07.rs
@@ -0,0 +1,91 @@
+use helpers::HashMap;
+
+#[derive(Default)]
+struct DirEntry<'a> {
+  dirs: HashMap<&'a str, DirEntry<'a>>,
+  files: HashMap<&'a str, FileEntry>,
+}
+
+struct FileEntry {
+  size: u32,
+}
+
+const ROOT_DIR: &str = "/";
+
+fn parse_entries(s: &str) -> DirEntry<'_> {
+  let mut cur = vec![(ROOT_DIR, DirEntry::default())];
+  let mut inside_ls = false;
+  for line in s.lines() {
+    match line.strip_prefix("$ ") {
+      None => {
+        assert!(inside_ls);
+        let (fst, snd) = line.split_once(' ').unwrap();
+        // no need to explicitly record dir entries from ls info
+        if fst != "dir" {
+          let size = fst.parse::<u32>().unwrap();
+          let (_, parent) = cur.last_mut().unwrap();
+          assert!(parent.files.insert(snd, FileEntry { size }).is_none());
+        }
+      }
+      Some(line) => {
+        if line == "ls" {
+          assert!(!inside_ls, "should not ls twice in a row");
+          inside_ls = true;
+        } else {
+          inside_ls = false;
+          let (cd, dir) = line.split_once(' ').unwrap();
+          assert_eq!(cd, "cd");
+          if dir == ".." {
+            let (name, done) = cur.pop().unwrap();
+            let (_, parent) = cur.last_mut().unwrap();
+            assert!(parent.dirs.insert(name, done).is_none());
+          } else {
+            cur.push((dir, DirEntry::default()));
+          }
+        }
+      }
+    }
+  }
+  loop {
+    let (name, done) = cur.pop().unwrap();
+    #[allow(clippy::single_match_else)]
+    match cur.last_mut() {
+      Some((_, parent)) => assert!(parent.dirs.insert(name, done).is_none()),
+      None => {
+        assert_eq!(name, ROOT_DIR);
+        return done;
+      }
+    }
+  }
+}
+
+pub fn p1(s: &str) -> u32 {
+  let e = parse_entries(s);
+  let mut ret = 0u32;
+  go(&mut ret, &e);
+  ret
+}
+
+fn go(ac: &mut u32, dir: &DirEntry<'_>) -> u32 {
+  let size: u32 = dir
+    .dirs
+    .values()
+    .map(|x| go(ac, x))
+    .chain(dir.files.values().map(|x| x.size))
+    .sum();
+  if size <= 100_000 {
+    *ac += size;
+  }
+  size
+}
+
+pub fn p2(_: &str) -> u32 {
+  0
+}
+
+#[test]
+fn t() {
+  let s = include_str!("input/d07.txt");
+  assert_eq!(p1(s), 1_428_881);
+  assert_eq!(p2(s), 0);
+}
